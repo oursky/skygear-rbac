1882f677380187d8d2a6f2e6af3b0b7a4fae58fe
Newman <newman@Newmans-MacBook-Pro.local>
2019-07-23 18:03:15 +0800

    Implement role assignment

--- handlers/role.go
+++ handlers/role.go
@@ -5,7 +5,18 @@ import (
 	"net/http"
 
 	casbin "github.com/casbin/casbin"
+	"github.com/gorilla/schema"
 )
+
+type RoleAssignment struct {
+	SubjectID string `json:"subjectId,omitempty" schema:"subjectId,omitempty"`
+	Role      string `json:"role,omitempty" schema:"role,omitempty"`
+}
+
+type RoleAssignmentInput struct {
+	SubjectID string `json:"subjectId,omitempty" schema:"subjectId,omitempty"`
+	Role      string `json:"role,omitempty" schema:"role,omitempty"`
+}
 
 type RoleHandler struct {
 	Enforcer *casbin.Enforcer
@@ -14,7 +25,21 @@ type RoleHandler struct {
 func (h *RoleHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
-		roles := h.Enforcer.GetImplicitRolesForUser("alice")
+		decoder := schema.NewDecoder()
+		filter := RoleAssignment{}
+		err := decoder.Decode(&filter, r.URL.Query())
+		if err != nil {
+			panic(err)
+		}
+
+		roles := h.Enforcer.GetImplicitRolesForUser(filter.SubjectID)
+		js, _ := json.Marshal(roles)
+		w.Write(js)
+	case http.MethodPost:
+		input := RoleAssignmentInput{}
+		json.NewDecoder(r.Body).Decode(&input)
+		h.Enforcer.AddRoleForUser(input.SubjectID, input.Role)
+		roles := h.Enforcer.GetImplicitRolesForUser(input.SubjectID)
 		js, _ := json.Marshal(roles)
 		w.Write(js)
 	}
--- handlers/role_test.go
+++ handlers/role_test.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"bytes"
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
@@ -19,7 +20,7 @@ func TestGetRoles(t *testing.T) {
 
 	req, _ := http.NewRequest("GET", server.URL, nil)
 	q := req.URL.Query()
-	q.Add("domain", "root")
+	q.Add("subjectId", "alice")
 	req.URL.RawQuery = q.Encode()
 
 	rec := httptest.NewRecorder()
@@ -30,6 +31,42 @@ func TestGetRoles(t *testing.T) {
 		t.Fatalf("Received non-200 response: %d\n", res.StatusCode)
 	}
 	policy, _ := json.Marshal([]string{"role:admin", "role:intern"})
+	expected := string(policy)
+	actual, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if expected != string(actual) {
+		t.Errorf("Expected the message '%s'\n", expected)
+		t.Errorf("Received '%s'\n", actual)
+	}
+}
+
+func TestAssignThenRemoveRole(t *testing.T) {
+	e := casbin.NewEnforcer("../model.conf")
+
+	handler := &RoleHandler{e}
+	server := httptest.NewServer(handler)
+	defer server.Close()
+
+	fakeRoleAssignment := RoleAssignmentInput{
+		Role:      "admin",
+		SubjectID: "billy",
+	}
+
+	body, _ := json.Marshal(fakeRoleAssignment)
+
+	req, _ := http.NewRequest("POST", server.URL, bytes.NewBuffer(body))
+	req.Header.Set("Content-Type", "application/json")
+
+	rec := httptest.NewRecorder()
+	handler.ServeHTTP(rec, req)
+	res := rec.Result()
+
+	if res.StatusCode != 200 {
+		t.Fatalf("Received non-200 response: %d\n", res.StatusCode)
+	}
+	policy, _ := json.Marshal([]string{fakeRoleAssignment.Role})
 	expected := string(policy)
 	actual, err := ioutil.ReadAll(res.Body)
 	if err != nil {
