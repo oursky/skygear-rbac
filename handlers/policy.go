b77b5fd14e4561c43123994a72e1e588065fd604
inizio <newman.kcchow@gmail.com>
2019-07-23 12:46:56 +0800

    Implement GET Policy

--- handlers/policy.go
+++ handlers/policy.go
@@ -1,12 +1,59 @@
 package handlers
 
 import (
+	"encoding/json"
 	"net/http"
+
+	casbin "github.com/casbin/casbin"
+	"github.com/gorilla/schema"
 )
 
+type Policy struct {
+	Domain    string `json:"domain,omitempty"`
+	SubjectID string `json:"subjectId,omitempty"`
+	ObjectID  string `json:"objectId,omitempty"`
+	Action    string `json:"action,omitempty"`
+}
+
+func PoliciesFromCasbin(raw [][]string) []Policy {
+	ps := []Policy{}
+
+	for _, s := range raw {
+		ps = append(ps, Policy{
+			Domain:    s[0],
+			SubjectID: s[1],
+			ObjectID:  s[2],
+			Action:    s[3],
+		})
+	}
+	return ps
+}
+
+type PolicyFilter struct {
+	Domain    string `schema:"domain,omitempty"`
+	SubjectID string `schema:"subjectId,omitempty"`
+	ObjectID  string `schema:"objectId,omitempty"`
+}
+
 type PolicyHandler struct {
+	Enforcer *casbin.Enforcer
 }
 
 func (h *PolicyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Policy CRUD"))
+	decoder := schema.NewDecoder()
+	filter := PolicyFilter{}
+	decoder.Decode(&filter, r.URL.Query())
+	// err := json.NewDecoder(r.Body).Decode(&filter)
+
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// policy := Policy{"Alex", "Form/123", "read"}
+	raw := h.Enforcer.GetFilteredPolicy(0, filter.Domain)
+	// raw := h.Enforcer.GetPolicy()
+	policies := PoliciesFromCasbin(raw)
+
+	js, _ := json.Marshal(policies)
+	w.Write(js)
 }
--- handlers/policy_test.go
+++ handlers/policy_test.go
@@ -1,31 +1,44 @@
 package handlers
 
 import (
-	"fmt"
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/casbin/casbin"
 )
 
 func TestPolicyHandler(t *testing.T) {
-	handler := &PolicyHandler{}
+	e := casbin.NewEnforcer("../model.conf")
+
+	e.AddPolicy("root", "admin", "data1", "write")
+
+	handler := &PolicyHandler{e}
 	server := httptest.NewServer(handler)
 	defer server.Close()
 
-	resp, err := http.Get(server.URL)
-	if err != nil {
-		t.Fatal(err)
+	req, _ := http.NewRequest("GET", server.URL, nil)
+	q := req.URL.Query()
+	q.Add("domain", "root")
+	req.URL.RawQuery = q.Encode()
+
+	rec := httptest.NewRecorder()
+	handler.ServeHTTP(rec, req)
+	res := rec.Result()
+
+	if res.StatusCode != 200 {
+		t.Fatalf("Received non-200 response: %d\n", res.StatusCode)
 	}
-	if resp.StatusCode != 200 {
-		t.Fatalf("Received non-200 response: %d\n", resp.StatusCode)
-	}
-	expected := fmt.Sprintf("Policy CRUD")
-	actual, err := ioutil.ReadAll(resp.Body)
+	policy, _ := json.Marshal([]Policy{Policy{"root", "admin", "data1", "write"}})
+	expected := string(policy)
+	actual, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		t.Fatal(err)
 	}
 	if expected != string(actual) {
 		t.Errorf("Expected the message '%s'\n", expected)
+		t.Errorf("Received '%s'\n", actual)
 	}
 }
