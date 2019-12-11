554f033e98317e3c7200cf365ce07c3630e2e87f
YinYin Chiu <cychiuae@connect.ust.hk>
2019-12-11 13:08:49 +0800

    Introduce app context

---
+++ pkg/context/app.go
@@ -0,0 +1,21 @@
+package context
+
+import (
+	"database/sql"
+
+	"github.com/casbin/casbin/v2"
+)
+
+// AppContext is used to pass services to request
+type AppContext struct {
+	db       *sql.DB
+	enforcer *casbin.Enforcer
+}
+
+// NewAppContext returns a new AppContext
+func NewAppContext(db *sql.DB, enforcer *casbin.Enforcer) AppContext {
+	return AppContext{
+		db:       db,
+		enforcer: enforcer,
+	}
+}
